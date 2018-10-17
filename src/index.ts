66bf62daadb31b0c4a1c53fe3f698b930ad8a8e1
Rob Van Gennip <ravangen@gmail.com>
2018-10-16 21:40:49 -0400

    Add Store interface

--- src/index.ts
+++ src/index.ts
@@ -28,17 +28,53 @@ export type RateLimitKeyGenerator<TContext> = (
   info: GraphQLResolveInfo,
 ) => string;
 
+// Store mirrors what express-rate-limit defines: https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/express-rate-limit
+export interface Store {
+  /**
+   * Increments the value in the underlying store for the given key.
+   * @param key The key to use as the unique identifier passed down from RateLimit.
+   * @param cb The callback issued when the underlying store is finished.
+   */
+  incr(key: string, cb: StoreIncrementCallback): void;
+  /**
+   * Decrements the value in the underlying store for the given key.
+   * @param key The key to use as the unique identifier passed down from RateLimit.
+   */
+  decrement(key: string): void;
+  /**
+   * Resets a value with the given key.
+   * @param key The key to use as the unique identifier passed down from RateLimit.
+   */
+  resetKey(key: string): void;
+}
+
+/**
+ * @param error Error (usually null). If null, it means operation was successfully attempted.
+ * @param hitCount Current number of occurrences for the given key.
+ */
+export type StoreIncrementCallback = (error?: {}, hitCount?: number) => void;
+
 export interface RateLimitConfig {
   directiveName?: string;
   keyGenerator?: RateLimitKeyGenerator<any>;
   onLimitReached?: Function;
+  store?: Store;
 }
 
-export const createRateLimitDirective = (config: RateLimitConfig = {
-  directiveName: 'rateLimit',
-  keyGenerator: (source: any, args: any, context: any, info: GraphQLResolveInfo) => info.fieldName,
-  onLimitReached: () => {},
-}): typeof SchemaDirectiveVisitor => {
+export const createRateLimitDirective = (
+  config: RateLimitConfig = {
+    directiveName: 'rateLimit',
+    keyGenerator: (
+      source: any,
+      args: any,
+      context: any,
+      info: GraphQLResolveInfo,
+    ) => info.fieldName,
+    onLimitReached: () => {
+      /* no-op */
+    },
+  },
+): typeof SchemaDirectiveVisitor => {
   class RateLimitDirective extends SchemaDirectiveVisitor {
     visitObject(object: GraphQLObjectType) {
       // Wrap fields for limiting that don't have their own @rateLimit
@@ -47,7 +83,9 @@ export const createRateLimitDirective = (config: RateLimitConfig = {
         const directives = field.astNode.directives;
         if (
           !directives ||
-          !directives.some(directive => directive.name.value === config.directiveName)
+          !directives.some(
+            directive => directive.name.value === config.directiveName,
+          )
         ) {
           this.limit(field);
         }
@@ -60,12 +98,11 @@ export const createRateLimitDirective = (config: RateLimitConfig = {
       // Rate limit this field
       const { resolve = defaultFieldResolver } = field;
       field.resolve = async (...args) => {
-        console.log(
-          `${config.keyGenerator(...args)}: ${this.args.max}/${this.args.period}`,
-        );
+        const key = config.keyGenerator(...args);
+        console.log(`${key}: ${this.args.max}/${this.args.period}`);
         return resolve.apply(this, args);
       };
     }
   }
   return RateLimitDirective;
-}
+};
